e4c83f491be2eae06a9234e7b4daf7f48bc34bae
sabareesh_vas29 <sabareeshvas@gmail.com>
2026-07-21 17:42:41 -0500

    fix base url

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const api = axios.create({ baseURL: 'https://ekadashi-backend.onrender.com' })
+export const api = axios.create({ baseURL: 'https://ekadashi-backend.onrender.com/api' })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('ekadashi_token')
